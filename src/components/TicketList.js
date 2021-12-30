939d1e58dacbf40bbbc3c254c83e4a5ccbd7fac6
Gabriel Ayala <gabeayala100@gmail.com>
2021-12-30 01:19:31 -0800

    Provide a loop that will list all ticket objects and their key value pairs.

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,18 +1,32 @@
 import React from "react";
-import reactDom from "react-dom";
 import Ticket from "./Ticket";
-
+const mainTicketList = [
+  {
+    names: 'Thato and Haly',
+    location: '3A',
+    issue: 'Firebase won\'t save record. Halp.'
+  },
+  {
+    names: 'Sleater and Kinney',
+    location: '4B',
+    issue: 'Prop types are throwing an error.'
+  },
+  {
+    names: 'Imani & Jacob',
+    location: '9F',
+    issue: 'Child component isn\'t rendering.'
+  }
+];
 function TicketList(){
   return (
     <React.Fragment>
-      <Ticket 
-        location="3A"
-        names="Thato and Haley"
-        issue="Firebase will not save record!"/>
-      <Ticket
-        location="4B"
-        names="Sleater and Kinney"
-        issue="Prop types are throwing an error."/>
+      <hr/>L
+        {mainTicketList.map((ticket, index) =>
+          <Ticket names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            key={index}/>
+        )}
     </React.Fragment>
     
 
