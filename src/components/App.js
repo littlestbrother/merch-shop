c6db8a26a77bfe2f8991bb49eff5fcde22b02a3f
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-03 18:57:06 -0800

    Import our parent file instead of ticketlist.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./Header";
-import TicketList from "./TicketList";
+import TicketControl from "./TicketControl";
 function App(){
   const name = "Thato";
   const name2 = "Haley";
   return (
     <React.Fragment>
       <Header />  
-      <TicketList />
+      <TicketControl />
     </React.Fragment>
   );
 }
