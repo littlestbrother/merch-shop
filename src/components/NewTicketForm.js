94b9ca5eec1e841893990f032598645d06fc13a4
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-07 11:02:12 -0800

    Add our new handle function so that we can utilize the onNewTicketCreation Prop.

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -5,9 +5,7 @@ import PropTypes from "prop-types";
 function NewTicketForm(props){
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-    console.log(event.target.names.value);
-    console.log(event.target.location.value);
-    console.log(event.target.issue.value);
+    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
   }
   return (
     <React.Fragment>
