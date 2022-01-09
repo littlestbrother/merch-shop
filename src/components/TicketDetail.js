c81c3831cd225b5119f02764db55fd0615053577
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-09 14:08:28 -0800

    Add the deconstructed props so that they may be called upon.

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -2,11 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 function TicketDetail(props){
   const { ticket, onClickingDelete} = props;//this is known as object destructuring. Its a way of copying whatever is in the bracketrs and setting it ewqual to teh variable so that it can be used in the component
+  // without the following above we cannot call on ticket and onclick below
   return (
     <React.Fragment>
       <h1>Ticket Detail</h1>
       <h3>{ticket.location} - {ticket.names}</h3>
       <p><em>{ticket.issue}</em></p>
+      <button onClick={ props.onClickingEdit}>UpdateTicket</button>
       <button onClick={() => onClickingDelete(ticket.id)}>Close Ticket</button>
       <hr/>
     </React.Fragment>
@@ -14,6 +16,7 @@ function TicketDetail(props){
 }
 TicketDetail.propTypes = {
   ticket: PropTypes.object,
-  onClickingDelete: PropTypes.func
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 };
 export default TicketDetail;
