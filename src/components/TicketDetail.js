fc7c89d2973dd2f25a64ab316d9593174fbc55bb
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-09 12:12:39 -0800

    Add a button that calls on the onClickinDelete which passes the ticket id as an arugemnt, which is possible because we destructured onClickingdelete .

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 function TicketDetail(props){
-  const { ticket } = props;//this is known as object destructuring
+  const { ticket, onClickingDelete} = props;//this is known as object destructuring
   return (
     <React.Fragment>
       <h1>Ticket Detail</h1>
       <h3>{ticket.location} - {ticket.names}</h3>
       <p><em>{ticket.issue}</em></p>
+      <button onClick={() => onClickingDelete(ticket.id)}>Close Ticket</button>
       <hr/>
     </React.Fragment>
   );
