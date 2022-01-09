8f7c932634af5a21cf61183ed9c5c27e5e2e6c1f
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-09 12:04:41 -0800

    Import proptypes.

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 function TicketDetail(props){
+  const { ticket } = props;//this is known as object destructuring
   return (
     <React.Fragment>
       <h1>Ticket Detail</h1>
+      <h3>{ticket.location} - {ticket.names}</h3>
+      <p><em>{ticket.issue}</em></p>
       <hr/>
     </React.Fragment>
   );
 }
-
+TicketDetail.propTypes = {
+  ticket: PropTypes.object
+};
 export default TicketDetail;
